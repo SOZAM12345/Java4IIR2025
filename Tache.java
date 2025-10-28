f57a88b40390b80167012ba27da6f9b24c8642e3
SOZAM12345 <158637691+SOZAM12345@users.noreply.github.com>
2025-10-28 11:21:31 +0100

    Add files via upload

---
+++ Tache.java
@@ -0,0 +1,21 @@
+package JavaAvance4IIR;
+import java.util.LinkedList;
+
+public class Tache {
+    private String description;
+    private boolean estComplete;
+
+    public Tache(String description) {
+        this.description = description;
+        this.estComplete = false;
+    }
+
+    public String getDescription() { return description; }
+    public boolean estComplete() { return estComplete; }
+    public void terminer() { estComplete = true; }
+
+    @Override
+    public String toString() {
+        return "Tache{" + "description='" + description + '\'' + ", estComplete=" + estComplete + '}';
+    }
+}
